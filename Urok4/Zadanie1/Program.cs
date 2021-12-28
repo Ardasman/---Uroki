26d699b381c35e716f6a573ee5f630b8fdc9727a
Ardasman <siberia_by@yahoo.com>
2021-12-28 16:01:52 +0300

    урок 4 2 задания

---
+++ Urok4/Zadanie1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Massiv a = new Massiv();
+int[] Mass = a.Massive(20);      // создаем массив, указываем размеер
+a.FillMass(Mass, -10000, 10000); // заполняем массив, указываем диапазон случайных чисел
+string str = a.Show(Mass);
+Console.WriteLine(str);
+int para = a.ParaThree(Mass); // считаем пары чисел, в которых только одно число делится на 3
+Console.WriteLine($" Количество пар: {para}");
---
+++ Urok4/Zadanie2/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] Mass = StaticClass.Massive(20);      // создаем массив, указываем размеер
+StaticClass.FillMass(Mass, -10000, 10000); // заполняем массив, указываем диапазон случайных чисел
+string str = StaticClass.Show(Mass);
+Console.WriteLine(str);
+// File.WriteAllText("Massv.txt", str);   // сзаписывае файл в массив
+int para = StaticClass.ParaThree(Mass); // считаем пары чисел, в которых только одно число делится на 3
+Console.WriteLine($" Количество пар: {para}");
