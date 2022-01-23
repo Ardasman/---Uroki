60dce6fa451b2997f1c029094d0735ff5f8a4001
Ardasman <siberia_by@yahoo.com>
2022-01-23 16:25:11 +0300

    Задание 1 8урок

---
+++ Urok8/Zadanie1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Астапов Алексей
+// С помощью рефлексии выведите все свойства структуры DateTime
+
+using System;
+using System.Reflection;
+
+
+Type t = typeof(DateTime);
+foreach (var prop in t.GetProperties())
+    Console.WriteLine(prop.Name);
